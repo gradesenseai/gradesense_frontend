06f7bf409a97eea6b67b8a2e0fa4ba4b8403b6e9
gradesenseai <konkolmark@icloud.com>
2025-08-21 10:52:51 -0500

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,9 @@
+import Head from 'next/head';
+// ...
+<Head>
+  <link rel="icon" href="/favicon.png" />
+</Head>
+  
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
